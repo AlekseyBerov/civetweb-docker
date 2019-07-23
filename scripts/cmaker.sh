e10744c5cfa6868ff4c283daea77ba3afe9f11f1
AlekseyBerov <brv0609@gmail.com>
2019-07-23 14:36:47 +0000

    root is too generic

--- scripts/cmaker.sh
+++ scripts/cmaker.sh
@@ -18,25 +18,24 @@ bash build;
 
 cd output/build; 
 grep -rl "PREFIX" *.cmake | xargs sed -i 's,"/usr/local","/tmp/artifacts/civet-pkg/usr",g';
-
 cmake -P cmake_install.cmake;
+
 cd /tmp/artifacts;
 
 mkdir civet-pkg/etc
 tee -a civet-pkg/etc/civetweb.conf << END
-document_root /var/www
+document_root /var/www/civet
 END
 
-
-mkdir -p civet-pkg/var/www;
-tee -a civet-pkg/var/www/index.html << END
+mkdir -p civet-pkg/var/www/civet;
+tee -a civet-pkg/var/www/civet/index.html << END
 <html>
-<title>CivetWeb docker image, how about that</title>
-<body>
+  <title>CivetWeb</title>
+    <body>
 
-<h2> some clear message </h2>
+      <h2> some clear message </h2>
 
-</body>
+   </body>
 </html>
 END
 
@@ -49,7 +48,6 @@ Maintainer: Aleksey Berov <krusual@live.com>
 Description: Civetweb
 END
 
-ls -la civet-pkg;
 dpkg-deb --build civet-pkg;
 
 
